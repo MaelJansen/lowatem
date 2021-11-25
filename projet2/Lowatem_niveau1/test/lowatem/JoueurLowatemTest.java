cf367c23ea261dc65369422a9319a3a219dea011
Jansen <mael.jansen@etu.u-bordeaux.fr>
2021-11-25 11:00:07 +0100

    niveaux 2 terminer

--- projet2/Lowatem_niveau1/test/lowatem/JoueurLowatemTest.java
+++ projet2/Lowatem_niveau1/test/lowatem/JoueurLowatemTest.java
@@ -16,7 +16,7 @@ public class JoueurLowatemTest {
      */
     @Test
     public void testActionsPossibles() {
-        //testActionsPossibles_niveau1();
+        testActionsPossibles_niveau1();
         testActionsPossibles_niveau2();
     }
 
